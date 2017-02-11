548f1c7675606c5fda365ab82e1fa36d8119795e
adamgobes <adamgobes@gmail.com>
2017-02-11 11:44:29 -0500

    update index.js

--- index.js
+++ index.js
@@ -61,6 +61,19 @@ const findOrCreateSession = (fbid) => {
     return sessionId;
 };
 
+
+const firstEntityValue = (entities, entity) => {
+    const val = entities && entities[entity] &&
+    Array.isArray(entities[entity]) &&
+    entities[entity].length > 0 &&
+    entities[entity][0].value
+    ;
+    if (!val) {
+        return null;
+    }
+    return typeof val === 'object' ? val.value : val;
+};
+
 const actions = {
     send({sessionId}, {text}) {
         const recipientId = sessions[sessionId].fbid;

58e7b75097b0ed6bd9ee701b90d6ad6edce79090
adamgobes <adamgobes@gmail.com>
2017-02-11 11:49:14 -0500

    update index.js

--- index.js
+++ index.js
@@ -97,11 +97,11 @@ const actions = {
         return new Promise(function(resolve, reject) {
             var location = firstEntityValue(entities, "location")
             if (location) {
-                context.forecast = 'sunny in ' + location; // we should call a weather API here
+                context.weather = 'sunny in ' + location; // we should call a weather API here
                 delete context.missingLocation;
             } else {
                 context.missingLocation = true;
-                delete context.forecast;
+                delete context.weather;
             }
             return resolve(context);
         });

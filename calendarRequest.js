c2c8f1895feebf232dc241ea8676df327db6d53f
adamgobes <adamgobes@gmail.com>
2017-02-18 00:12:25 -0500

    update heroku

--- calendarRequest.js
+++ calendarRequest.js
@@ -8,7 +8,7 @@ console.log(today.add(2, "day").toISOString());
 const request = (date, sport) => {
     const calendarId = "athleticsmcgill@gmail.com";
     let today = moment(date).toISOString();
-    let tomorrow = moment(date).toISOString();
+    let tomorrow = moment(date).add(1, "days").toISOString();
     console.log(today, tomorrow);
     const myKey = "AIzaSyBVniGKarvjET4CVH5OisnQ0NxJsH09L2w";
     let url = "https://www.googleapis.com/calendar/v3/calendars/" + calendarId + "/events?key=" + myKey + "&timeMin=" + today + "&timeMax=" + tomorrow + "&showDeleted=false&singleEvents=true&orderBy=startTime";

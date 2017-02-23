95b629c5a0ae34d86b6c8ef076175b1cf0948ccd
adamgobes <adamgobes@gmail.com>
2017-02-22 22:09:40 -0500

    update heroku

--- calendarRequest.js
+++ calendarRequest.js
@@ -2,14 +2,10 @@ const axios = require('axios');
 const moment = require('moment');
 
 
-var today = moment("2017-02-18T00:00:00.000-08:00");
-console.log(today.add(2, "day").toISOString());
-
 const requestTimes = (date) => {
     const calendarId = "athleticsmcgill@gmail.com";
     let desiredDate = moment(date).toISOString();
     let dayAfter = moment(date).add(1, "days").toISOString();
-    console.log(desiredDate, dayAfter);
     const myKey = "AIzaSyBVniGKarvjET4CVH5OisnQ0NxJsH09L2w";
     let url = "https://www.googleapis.com/calendar/v3/calendars/" + calendarId + "/events?key=" + myKey + "&timeMin=" + desiredDate + "&timeMax=" + dayAfter + "&showDeleted=false&singleEvents=true&orderBy=startTime";
     return axios.get(url);
@@ -17,7 +13,15 @@ const requestTimes = (date) => {
 
 const filter = (events, sport) => {
     let returnString = "";
-    let eventString = "Varsity Badminton";
+    let eventString;
+    switch (sport) {
+        case "basketball":
+            eventString = "Rec Basketball";
+            break;
+        case "badminton":
+            eventString = "Rec Badminton";
+            break;
+    }
     for (var i = 0; i < events.length; i++) {
         if (events[i].summary.includes(eventString)) {
             returnString += " " + events[i].summary + " " + events[i].start.dateTime;

0eb1746755edfb57ec05218de6bf58c5dc551fa9
adamgobes <adamgobes@gmail.com>
2017-02-22 22:45:13 -0500

    update heroku

--- calendarRequest.js
+++ calendarRequest.js
@@ -24,7 +24,7 @@ const filter = (events, sport) => {
     }
     for (var i = 0; i < events.length; i++) {
         if (events[i].summary.includes(eventString)) {
-            console.log(moment(events[i].start.dateTime, moment.ISO_8601));
+            console.log(moment("2017-02-26T14:00:00-05:00", moment.ISO_8601));
             filteredList.push({
                 start: moment(events[i].start.dateTime).hour(),
                 end: moment(events[i].end.dateTime).hour()

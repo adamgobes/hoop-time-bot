b29dac253035480db6415b38efa2d6b9a315b6d9
adamgobes <adamgobes@gmail.com>
2017-03-26 21:41:46 -0400

    update heroku

--- index.js
+++ index.js
@@ -135,13 +135,6 @@ const actions = {
                 return resolve(context);
             }
         });
-    },
-
-    unhandledRequest({context, entities}) {
-        return new Promise(function(resolve, reject) {
-            context.response = "Sorry, I don't know how to handle that";
-            return resolve(context);
-        });
     }
 };
 

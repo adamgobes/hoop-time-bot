90ee61b65c57037eb918f291200de02de8e0be3b
adamgobes <adamgobes@gmail.com>
2017-04-17 00:01:08 -0400

    update heroku

--- webhook.js
+++ webhook.js
@@ -96,6 +96,7 @@ app.post('/ai', (req, res) => {
     } else if (req.body.result.action == "find_gym") {
         let date = req.body.result.parameters.date;
         let facility = req.body.result.parameters.facility;
+        console.log(date, facility);
         let times = calendarRequest.requestTimes(date, facility).then(function(response) {
             let msg = calendarRequest.generateOpenGymTimes(response.data.items);
             return res.json({

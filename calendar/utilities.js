58730702c86152d52e8caf1500be4926f4efa689
Adam Gobran <adamgobes@gmail.com>
2017-09-07 18:21:17 -0400

    update heroku

--- calendar/utilities.js
+++ calendar/utilities.js
@@ -1,5 +1,7 @@
-export const parseDate = (date) => {
+const parseDate = (date) => {
     let indexOfT = date.indexOf("T");
     let parsed = date.substring(0, indexOfT) + " " + date.substring(indexOfT + 1, 16);
     return parsed;
 }
+
+module.exports = { parseDate };
